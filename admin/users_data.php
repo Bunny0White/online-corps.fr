d1712cc64141cd1d3fd080b92635b7a0e67c3740
Bunny0White <ylan0ratineau@gmail.com>
2024-04-05 14:51:14 +0200

    Update users_data.php

--- admin/users_data.php
+++ admin/users_data.php
@@ -1,18 +1,13 @@
 <?php
 $users = array (
-  'DamsPTC' => 
-  array (
-    'password' => '0fc1fd183c228bf8',
-    'role' => 'admin',
-  ),
   'Bunny_White' => 
   array (
-    'password' => 'Bunny_White',
+    'password' => 'Nohlan11@p',
     'role' => 'admin',
   ),
   'Damien' => 
   array (
-    'password' => 'Dams',
+    'password' => 'Damain',
     'role' => 'user',
   ),
 );
